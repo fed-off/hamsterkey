9a12603a3106deab11694f32762e122b2ca83573
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-07-22 23:37:34 +0300

    update 22.07

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const gridSize = 6;
 let blockSize;
 const blocks = [
-    { id: 'red1', x: 0, y: 0, width: 1, height: 2, color: 'red' },
-    { id: 'red2', x: 2, y: 1, width: 1, height: 2, color: 'red' },
-    { id: 'red3', x: 5, y: 1, width: 1, height: 2, color: 'red' },
-    { id: 'red4', x: 3, y: 2, width: 1, height: 3, color: 'red' },
-    { id: 'red5', x: 1, y: 3, width: 1, height: 3, color: 'red' },
-    { id: 'red6', x: 4, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'green1', x: 1, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green2', x: 4, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green3', x: 3, y: 1, width: 2, height: 1, color: 'green' },
-    { id: 'green4', x: 4, y: 3, width: 2, height: 1, color: 'green' },
-    { id: 'green5', x: 2, y: 5, width: 2, height: 1, color: 'green' },
-    { id: 'key', x: 0, y: 2, width: 2, height: 1, color: 'key' }
+    { id: 'red1', x: 0, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red2', x: 1, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red3', x: 2, y: 0, width: 1, height: 2, color: 'red' },
+    { id: 'red4', x: 4, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red5', x: 5, y: 1, width: 1, height: 3, color: 'red' },
+    { id: 'red6', x: 3, y: 3, width: 1, height: 3, color: 'red' },
+    { id: 'green1', x: 0, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green2', x: 3, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green3', x: 4, y: 4, width: 2, height: 1, color: 'green' },
+    { id: 'key', x: 2, y: 2, width: 2, height: 1, color: 'key' }
 ];
 
 
