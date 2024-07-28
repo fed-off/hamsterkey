2b33f2e27859e20c37b743e7c8c2c2598829038f
Sergei Fedorov <imfedr@gmail.com>
2024-07-28 22:06:11 +0200

    Update puzzle 10

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const gridSize = 6;
 let blockSize;
 const blocks = [
-    { id: 'red1', x: 1, y: 1, width: 1, height: 2, color: 'red' },
-    { id: 'red2', x: 4, y: 0, width: 1, height: 3, color: 'red' },
-    { id: 'red3', x: 5, y: 0, width: 1, height: 2, color: 'red' },
-    { id: 'red4', x: 2, y: 3, width: 1, height: 2, color: 'red' },
+    { id: 'red1', x: 0, y: 0, width: 1, height: 2, color: 'red' },
+    { id: 'red2', x: 4, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red3', x: 5, y: 1, width: 1, height: 3, color: 'red' },
+    { id: 'red4', x: 2, y: 2, width: 1, height: 3, color: 'red' },
     { id: 'red5', x: 3, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'green1', x: 0, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green2', x: 0, y: 4, width: 2, height: 1, color: 'green' },
-    { id: 'green3', x: 0, y: 5, width: 3, height: 1, color: 'green' },
-    { id: 'green4', x: 2, y: 1, width: 2, height: 1, color: 'green' },
-    { id: 'green5', x: 4, y: 3, width: 2, height: 1, color: 'green' },
-    { id: 'key', x: 2, y: 2, width: 2, height: 1, color: 'key' }
+    { id: 'green1', x: 0, y: 4, width: 2, height: 1, color: 'green' },
+    { id: 'green2', x: 0, y: 5, width: 3, height: 1, color: 'green' },
+    { id: 'green3', x: 1, y: 1, width: 3, height: 1, color: 'green' },
+    { id: 'green4', x: 3, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green5', x: 3, y: 3, width: 2, height: 1, color: 'green' },
+    { id: 'green6', x: 4, y: 4, width: 2, height: 1, color: 'green' },
+    { id: 'key', x: 0, y: 2, width: 2, height: 1, color: 'key' }
 ];
 
 
@@ -110,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
             selectedBlock.dataset.x === '4') {
                 if (totalSeconds <= 15) {
                     stopTimer();
-                    // alert(`Поздравляем! Ваше время ${totalSeconds} секунд!\nСделайте скриншот и отправьте его в телеграм канал, чтобы поучаствовать в розыгрыше!`);
+                    alert(`Поздравляем! Ваше время ${totalSeconds} секунд!\nСделайте скриншот и отправьте его в телеграм канал, чтобы поучаствовать в розыгрыше!`);
                 }
                 stopTimer();
             }
@@ -180,7 +181,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateProgressBar() {
         const now = new Date();
-        const targetTime = new Date(Date.UTC(2024, 6, 28, 20));
+        const targetTime = new Date(Date.UTC(2024, 6, 29, 20));
 
         const startOfDay = new Date(targetTime);
         startOfDay.setUTCDate(startOfDay.getUTCDate() - 1);
