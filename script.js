52ad95f1731f4ae9123f678a208e9dd29de8f145
Sergei Fedorov <imfedr@gmail.com>
2024-07-29 22:05:26 +0200

    Update puzzle 11

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const gridSize = 6;
 let blockSize;
 const blocks = [
     { id: 'red1', x: 0, y: 0, width: 1, height: 2, color: 'red' },
-    { id: 'red2', x: 4, y: 1, width: 1, height: 2, color: 'red' },
-    { id: 'red3', x: 5, y: 1, width: 1, height: 3, color: 'red' },
-    { id: 'red4', x: 2, y: 2, width: 1, height: 3, color: 'red' },
-    { id: 'red5', x: 3, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'green1', x: 0, y: 4, width: 2, height: 1, color: 'green' },
-    { id: 'green2', x: 0, y: 5, width: 3, height: 1, color: 'green' },
-    { id: 'green3', x: 1, y: 1, width: 3, height: 1, color: 'green' },
-    { id: 'green4', x: 3, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green5', x: 3, y: 3, width: 2, height: 1, color: 'green' },
-    { id: 'green6', x: 4, y: 4, width: 2, height: 1, color: 'green' },
+    { id: 'red2', x: 1, y: 0, width: 1, height: 2, color: 'red' },
+    { id: 'red3', x: 2, y: 2, width: 1, height: 3, color: 'red' },
+    { id: 'red4', x: 3, y: 4, width: 1, height: 2, color: 'red' },
+    { id: 'red5', x: 5, y: 1, width: 1, height: 3, color: 'red' },
+    { id: 'green1', x: 0, y: 5, width: 3, height: 1, color: 'green' },
+    { id: 'green2', x: 2, y: 1, width: 3, height: 1, color: 'green' },
+    { id: 'green3', x: 3, y: 3, width: 2, height: 1, color: 'green' },
+    { id: 'green4', x: 4, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green5', x: 4, y: 4, width: 2, height: 1, color: 'green' },
     { id: 'key', x: 0, y: 2, width: 2, height: 1, color: 'key' }
 ];
 
@@ -185,7 +184,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateProgressBar() {
         const now = new Date();
-        const targetTime = new Date(Date.UTC(2024, 6, 29, 20));
+        const targetTime = new Date(Date.UTC(2024, 6, 30, 20));
 
         const startOfDay = new Date(targetTime);
         startOfDay.setUTCDate(startOfDay.getUTCDate() - 1);
