4571cd96b137d8319927e7a6496de5357f6af475
Sergei Fedorov <imfedr@gmail.com>
2024-07-30 22:03:59 +0200

    Update puzzle 12

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const gridSize = 6;
 let blockSize;
 const blocks = [
-    { id: 'red1', x: 0, y: 0, width: 1, height: 2, color: 'red' },
-    { id: 'red2', x: 1, y: 0, width: 1, height: 2, color: 'red' },
-    { id: 'red3', x: 2, y: 2, width: 1, height: 3, color: 'red' },
-    { id: 'red4', x: 3, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'red5', x: 5, y: 1, width: 1, height: 3, color: 'red' },
-    { id: 'green1', x: 0, y: 5, width: 3, height: 1, color: 'green' },
-    { id: 'green2', x: 2, y: 1, width: 3, height: 1, color: 'green' },
-    { id: 'green3', x: 3, y: 3, width: 2, height: 1, color: 'green' },
-    { id: 'green4', x: 4, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green5', x: 4, y: 4, width: 2, height: 1, color: 'green' },
-    { id: 'key', x: 0, y: 2, width: 2, height: 1, color: 'key' }
+    { id: 'red1', x: 2, y: 0, width: 1, height: 2, color: 'red' },
+    { id: 'red2', x: 1, y: 3, width: 1, height: 3, color: 'red' },
+    { id: 'red3', x: 2, y: 3, width: 1, height: 2, color: 'red' },
+    { id: 'red4', x: 4, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red5', x: 5, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red6', x: 5, y: 3, width: 1, height: 3, color: 'red' },
+    { id: 'green1', x: 0, y: 1, width: 2, height: 1, color: 'green' },
+    { id: 'green2', x: 0, y: 2, width: 2, height: 1, color: 'green' },
+    { id: 'green3', x: 2, y: 5, width: 3, height: 1, color: 'green' },
+    { id: 'green4', x: 3, y: 3, width: 2, height: 1, color: 'green' },
+    { id: 'green5', x: 4, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'key', x: 2, y: 2, width: 2, height: 1, color: 'key' }
 ];
 
 
@@ -229,7 +230,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateProgressBar() {
         const now = new Date();
-        const targetTime = new Date(Date.UTC(2024, 6, 30, 20));
+        const targetTime = new Date(Date.UTC(2024, 6, 31, 20));
 
         const startOfDay = new Date(targetTime);
         startOfDay.setUTCDate(startOfDay.getUTCDate() - 1);
