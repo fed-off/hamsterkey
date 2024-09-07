ca6ea84ae97d8d2a6c50afbc60866f26290a182f
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-09-07 23:11:43 +0300

    Update 07.09 wo video

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ import farmToken from './farm.js';
 const gridSize = 6;
 let blockSize;
 const blocks = [
-    { id: 'red1', x: 1, y: 1, width: 1, height: 3, color: 'red' },
-    { id: 'red2', x: 2, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'red3', x: 3, y: 4, width: 1, height: 2, color: 'red' },
-    { id: 'red4', x: 4, y: 1, width: 1, height: 2, color: 'red' },
-    { id: 'red5', x: 5, y: 1, width: 1, height: 3, color: 'red' },
+    { id: 'red1', x: 0, y: 3, width: 1, height: 3, color: 'red' },
+    { id: 'red2', x: 1, y: 4, width: 1, height: 2, color: 'red' },
+    { id: 'red3', x: 2, y: 1, width: 1, height: 2, color: 'red' },
+    { id: 'red4', x: 3, y: 1, width: 1, height: 3, color: 'red' },
+    { id: 'red5', x: 5, y: 3, width: 1, height: 2, color: 'red' },
     // { id: 'red6', x: 5, y: 1, width: 1, height: 3, color: 'red' },
     // { id: 'red7', x: 5, y: 3, width: 1, height: 2, color: 'red' },
-    { id: 'green1', x: 1, y: 0, width: 3, height: 1, color: 'green' },
-    { id: 'green2', x: 4, y: 0, width: 2, height: 1, color: 'green' },
-    { id: 'green3', x: 3, y: 3, width: 2, height: 1, color: 'green' },
-    { id: 'green4', x: 0, y: 5, width: 2, height: 1, color: 'green' },
-    { id: 'green5', x: 4, y: 5, width: 2, height: 1, color: 'green' },
-    // { id: 'green6', x: 4, y: 5, width: 2, height: 1, color: 'green' },
-    { id: 'key', x: 2, y: 2, width: 2, height: 1, color: 'key' }
+    { id: 'green1', x: 0, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green2', x: 1, y: 3, width: 2, height: 1, color: 'green' },
+    { id: 'green3', x: 2, y: 0, width: 2, height: 1, color: 'green' },
+    { id: 'green4', x: 3, y: 4, width: 2, height: 1, color: 'green' },
+    { id: 'green5', x: 3, y: 5, width: 3, height: 1, color: 'green' },
+    { id: 'green6', x: 4, y: 1, width: 2, height: 1, color: 'green' },
+    { id: 'key', x: 0, y: 2, width: 2, height: 1, color: 'key' }
 ];
 
 const API_URL = window.location.hostname === 'localhost' ?
